388b3729a544b982f4fc5db9d6d251b48cd92137
Gul <gngulnayab@gmail.com>
2025-05-18 17:59:32 -0700

    updated jest with babel for more detailed configuration

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
